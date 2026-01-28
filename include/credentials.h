c1d8d4f021a72415188f90102b5942f9435ffa64
esteban <esteban@local>
2026-01-28 18:35:07 -0300

    Update Wi-Fi SSID and password

---
+++ include/credentials.h
@@ -0,0 +1,13 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+#define WIFI_SSID "juangadospuntocuatrola"
+#define WIFI_PASS "ASDFGH"
+
+#define AIO_USERNAME "Elmonoloco12312"
+#define AIO_KEY "REPLACE_WITH_REAL_KEY"
+
+#define IO_USERNAME AIO_USERNAME
+#define IO_KEY AIO_KEY
+
+#endif // CREDENTIALS_H
